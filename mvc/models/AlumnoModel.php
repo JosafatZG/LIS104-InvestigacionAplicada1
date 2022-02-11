50cd2b0b267b15f1fcecdceaf2b15c946007129e
Melvin Robles <56745314+Melvin-Robles@users.noreply.github.com>
2022-02-11 02:14:52 -0600

    Programación de AlumnoModel.php

---
+++ mvc/models/AlumnoModel.php
@@ -0,0 +1,54 @@
+<?php
+
+class alumnos_model {
+		
+    private $db;
+    private $vehiculos;
+    
+    public function __construct(){
+        $this->db = Conectar::conexion();
+        $this->alumnos = array();
+    }
+
+
+    public function get_alumnos()
+		{
+			$sql = "SELECT * FROM alumnos";
+			$resultado = $this->db->query($sql);
+			while($row = $resultado->fetch_assoc())
+			{
+				$this->alumnos[] = $row;
+			}
+			return $this->alumnos;
+		}
+
+        public function insertar($carnet, $dui, $nombre, $apellido, $carrera){
+			
+			$resultado = $this->db->query("INSERT INTO alumnos (carnet, dui, nombre, apellido, carrera) VALUES ('$carnet', '$dui', '$nombre', '$apellido', '$carrera')");
+			
+		}
+		
+		public function modificar($carnet, $dui, $nombre, $apellido, $carrera){
+			
+			$resultado = $this->db->query("UPDATE alumnos SET dui='$dui', nombre='$nombre', apellido='$apellido', carrera='$carrera' WHERE carnet = '$carnet'");			
+		}
+		
+		public function eliminar($carnet){
+			
+			$resultado = $this->db->query("DELETE FROM alumnos WHERE carnet = '$carnet'");
+			
+		}
+		
+		public function get_vehiculo($carnet)
+		{
+			$sql = "SELECT * FROM alumnos WHERE carnet='$carnet' LIMIT 1";
+			$resultado = $this->db->query($sql);
+			$row = $resultado->fetch_assoc();
+
+			return $row;
+		}
+	} 
+
+
+
+?>
